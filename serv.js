fb0469eab6c6ba2d00c12269ed4a13195130b80d
John <jpaine@geiger.com>
2018-05-25 01:08:26 -0400

    updates

--- serv.js
+++ serv.js
@@ -9,31 +9,43 @@ var fs = require('fs');
 
 
 
-getTasks(function(err, tasks) {
+getTasks(function(err, remoteTasks) {
     if (err) {
         console.log(err)
         return
     }
-    console.log(tasks);
-})
+    console.log('Received: ' + remoteTasks.tasks.length + " tasks from remote");
+   // compareLists(tasks.tasks, readTasks().tasks);
+   readTasks(function(err, localTasks) {
+       if (err) {
+           console.log(err)
+           return
+       }
+       compareLists(remoteTasks.tasks, localTasks.tasks);
+   });
+}) 
 
-
-
-
-
+function compareLists(remote, local) {
+    console.log('comparing remote to local');
+  /*  for (var r in remote) {
+        console.log(remote[r].id);
+    } */
+     for (var l in local) {
+        console.log(local[l].id);
+    }
+}
 function getTasks(callback) {
     teamwork.getTasks(config.api_key, 175854, function(err, tl) {
     
         if (err) {
-        callback(err, null);
-        return
+            callback(err, null);
+            return
         }
         var tasklist = JSON.parse(tl)['todo-items'];
         var obj = {
             tasks : []
         };
         for (var task in tasklist) {
-
             var t = {
                 id : tasklist[task].id,
                 content : tasklist[task].content,
@@ -45,8 +57,6 @@ function getTasks(callback) {
             }
             obj.tasks.push(t);
         }
-
-        console.log('Received ' + obj.tasks.length + ' tasks');
         callback(null, obj);
     });
 };
@@ -57,9 +67,27 @@ function writeTasks(obj) {
         if (err) {
            console.log(err)
         }
-       
     });
 }
+function readTasks(callback) {
+    fs.readFile('tasks.json', 'utf8', function readFileCallback(err, data){
+        if (err){
+            callback(err, null)
+            return;
+        } 
+            obj = JSON.parse(data); 
+
+            callback(null, obj)
+            
+            //now it an object
+         /*   obj.table.push({id: 2, square:3}); //add some data
+            json = JSON.stringify(obj); //convert it back to json
+            fs.writeFile('myjsonfile.json', json, 'utf8', callback); // write it back 
+         */
+    
+        });
+    }
+
 
 
 
